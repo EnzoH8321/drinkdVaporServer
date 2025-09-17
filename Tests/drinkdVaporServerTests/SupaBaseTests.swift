03c3eff192eb970de0fe3ca0ecb60c3c677e8992
EnzoHerrera <enzoherrera@cattlekrush.com>
2025-09-17 13:31:12 -0700

    Adds more test cases to supabase

--- Tests/drinkdVaporServerTests/SupaBaseTests.swift
+++ Tests/drinkdVaporServerTests/SupaBaseTests.swift
@@ -17,11 +17,6 @@ struct SupaBaseTests {
     let supabase: SupaBase
     let client: SupabaseClient
 
-    let partyLeader = FakePartyLeader()
-    let guest = FakeGuest()
-    let party = FakeParty()
-    let restaurant = FakeRestaurant()
-
     init() {
         let client = SupabaseClient(supabaseURL: URL(string: "http://localhost:54321")!, supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0")
 
@@ -30,8 +25,6 @@ struct SupaBaseTests {
         self.supabase = supabase
         self.client = client
     }
-
-
 
     @Test("Create a party")
     func createParty_Test() async throws {
@@ -59,13 +52,13 @@ struct SupaBaseTests {
         do {
 
             try await stubCreateAParty()
-            try await stubCreateAGuest(partyID: party.id)
+            try await stubCreateAGuest()
 
-            let req = LeavePartyRequest(userID: partyLeader.id)
+            let req = LeavePartyRequest(userID: FakePartyLeader.id)
 
-            try await supabase.leavePartyAsHost(req, partyID: party.id)
+            try await supabase.leavePartyAsHost(req, partyID: FakeParty.id)
 
-            try await stubCleanupUser(guest.id)
+            try await stubCleanupUser(FakeGuest.id)
             try await stubCleanupParty()
         } catch {
             Issue.record(error)
@@ -79,13 +72,13 @@ struct SupaBaseTests {
         do {
 
             try await stubCreateAParty()
-            try await stubCreateAGuest(partyID: party.id)
-            let req = LeavePartyRequest(userID: guest.id)
+            try await stubCreateAGuest()
+            let req = LeavePartyRequest(userID: FakeGuest.id)
 
             try await supabase.leavePartyAsGuest(req)
 
             try await stubCleanupParty()
-            try await stubCleanupUser(guest.id)
+            try await stubCleanupUser(FakeGuest.id)
         } catch {
             Issue.record(error)
         }
@@ -96,16 +89,16 @@ struct SupaBaseTests {
     func joinParty_Test() async throws {
         do {
             let _ = try await stubCreateAParty()
-            let req = JoinPartyRequest(userID: guest.id, username: guest.username, partyCode: party.code)
+            let req = JoinPartyRequest(userID: FakeGuest.id, username: FakeGuest.username, partyCode: FakeParty.code)
 
             let parties = try await supabase.joinParty(req)
-            #expect(parties.code == party.code)
-            #expect(parties.party_leader == partyLeader.id)
+            #expect(parties.code == FakeParty.code)
+            #expect(parties.party_leader == FakePartyLeader.id)
             #expect(parties.party_name == "Party007")
-            #expect(parties.id == party.id)
+            #expect(parties.id == FakeParty.id)
 
             try await stubCleanupParty()
-            try await stubCleanupUser(guest.id)
+            try await stubCleanupUser(FakeGuest.id)
 
         } catch {
             Issue.record(error)
@@ -119,7 +112,7 @@ struct SupaBaseTests {
         do {
             try await stubCreateAParty()
             try await stubCreateARestaurant()
-            let req = UpdateRatingRequest(partyID: party.id, userID: partyLeader.id, userName: partyLeader.username, restaurantName: restaurant.name, rating: restaurant.rating, imageURL: restaurant.imageURL)
+            let req = UpdateRatingRequest(partyID: FakeParty.id, userID: FakePartyLeader.id, userName: FakePartyLeader.username, restaurantName: FakeRestaurant.name, rating: FakeRestaurant.rating, imageURL: FakeRestaurant.imageURL)
 
             try await supabase.updateRestaurantRating(req)
 
@@ -128,9 +121,27 @@ struct SupaBaseTests {
         } catch {
             Issue.record(error)
         }
-
     }
 
+    @Test("Send a Message")
+    func sendMessage_Test() async throws {
+
+        do {
+            try await stubCreateAParty()
+            try await stubCreateAGuest()
+
+            let req = SendMessageRequest(userID: FakeGuest.id, username: FakeGuest.username, partyID: FakeParty.id, message: "TestMessage007")
+            let messageID = UUID()
+
+            try await supabase.sendMessage(req, messageID: messageID)
+
+            try await stubCleanupParty()
+            try await stubCleanupUser(FakeGuest.id)
+        } catch {
+            Issue.record(error)
+        }
+
+    }
 
 }
 
@@ -139,46 +150,44 @@ extension SupaBaseTests {
 
     // Stub create a party
     private func stubCreateAParty() async throws  {
-        let table = PartiesTable(id: party.id, party_name: party.name, party_leader: partyLeader.id, date_created: Date().ISO8601Format(), code: party.code, restaurants_url: party.restaurantURL)
+        let table = PartiesTable(id: FakeParty.id, party_name: FakeParty.name, party_leader: FakePartyLeader.id, date_created: Date().ISO8601Format(), code: FakeParty.code, restaurants_url: FakeParty.restaurantURL)
 
         do {
             try await client.from(TableTypes.parties.tableName).upsert(table).execute()
-
         }
 
     }
 
     // Stub create a host
-    private func stubCreateAHost(partyID: UUID) async throws  {
-        let host = UsersTable(id: partyLeader.id, username: partyLeader.username, date_created: Date().ISO8601Format(), memberOfParty: partyID)
+    private func stubCreateAHost() async throws  {
+        let host = UsersTable(id: FakePartyLeader.id, username: FakePartyLeader.username, date_created: Date().ISO8601Format(), memberOfParty: FakeParty.id)
         try await client.from(TableTypes.users.tableName).upsert(host).execute()
     }
 
-    private func stubCreateAGuest(partyID: UUID) async throws  {
-        let guest = UsersTable(id: guest.id, username: guest.username, date_created: Date().ISO8601Format(), memberOfParty: partyID)
+    private func stubCreateAGuest() async throws  {
+        let guest = UsersTable(id: FakeGuest.id, username: FakeGuest.username, date_created: Date().ISO8601Format(), memberOfParty: FakeParty.id)
         try await client.from(TableTypes.users.tableName).upsert(guest).execute()
      }
 
     private func stubCreateARestaurant() async throws {
-        let restaurant = RatedRestaurantsTable(id: restaurant.id, partyID: restaurant.partyID, userID: partyLeader.id, userName: partyLeader.username, restaurantName: restaurant.name, rating: restaurant.rating, imageURL: restaurant.imageURL)
+        let restaurant = RatedRestaurantsTable(id: FakeRestaurant.id, partyID: FakeRestaurant.partyID, userID: FakePartyLeader.id, userName: FakePartyLeader.username, restaurantName: FakeRestaurant.name, rating: FakeRestaurant.rating, imageURL: FakeRestaurant.imageURL)
 
         try await client.from(TableTypes.ratedRestaurants.tableName).upsert(restaurant).execute()
-
     }
 
     // Delete created row
     private func stubCleanup(_ userLevel: UserLevel) async {
-        let userID = (userLevel == .leader) ? partyLeader.id : guest.id
+        let userID = (userLevel == .leader) ? FakePartyLeader.id : FakeGuest.id
 
         do {
-            try await client.from(TableTypes.parties.tableName).delete().eq("id", value: party.id).execute()
+            try await client.from(TableTypes.parties.tableName).delete().eq("id", value: FakeParty.id).execute()
             try await client.from(TableTypes.users.tableName).delete().eq("id", value: userID).execute()
         } catch {
             Issue.record("deleteDataFromTable failed: \(error)")
         }
     }
 
-    private func stubCleanupParty(partyID: UUID = FakeParty().id) async throws {
+    private func stubCleanupParty(partyID: UUID = FakeParty.id) async throws {
         do {
             try await client.from(TableTypes.parties.tableName).delete().eq("id", value: partyID).execute()
         }
@@ -192,35 +201,35 @@ extension SupaBaseTests {
 
     private func stubCleanupRestaurant() async throws {
         do {
-            try await client.from(TableTypes.users.tableName).delete().eq("id", value: restaurant.id).execute()
+            try await client.from(TableTypes.users.tableName).delete().eq("id", value: FakeRestaurant.id).execute()
         }
     }
 }
 
 extension SupaBaseTests {
     struct FakePartyLeader {
-        let username = "Leader007"
-        let id: UUID = UUID(uuidString: "F47AC10B-58CC-4372-A567-0E02B2C3D479")!
+        static let username = "Leader007"
+        static let id: UUID = UUID(uuidString: "F47AC10B-58CC-4372-A567-0E02B2C3D479")!
     }
 
     struct FakeGuest {
-        let username = "Guest007"
-        let id: UUID = UUID(uuidString: "C3D4E5F6-1A2B-4C7D-8E9F-0A1B2C3D4E5F")!
+        static let username = "Guest007"
+        static let id: UUID = UUID(uuidString: "C3D4E5F6-1A2B-4C7D-8E9F-0A1B2C3D4E5F")!
     }
 
     struct FakeParty {
-        let id: UUID = UUID(uuidString: "C3D3E5F6-1A2B-4C7D-8E9F-0A2B2C3D4E6F")!
-        let restaurantURL = "https://api.yelp.com/v3/businesses/search?categories=bars&latitude=37.774292458506686&longitude=-122.21621476154564&limit=10"
-        let name = "Party007"
-        let code = 345789
+        static let id: UUID = UUID(uuidString: "C3D3E5F6-1A2B-4C7D-8E9F-0A2B2C3D4E6F")!
+        static let restaurantURL = "https://api.yelp.com/v3/businesses/search?categories=bars&latitude=37.774292458506686&longitude=-122.21621476154564&limit=10"
+        static let name = "Party007"
+        static let code = 345789
     }
 
     struct FakeRestaurant {
-        let name = "BestRestaurant007"
-        let rating = 4
-        let imageURL = "https://s3-media0.fl.yelpcdn.com/bphoto/rKctRFj8diqswEkATTDC5g/o.jpg"
-        let partyID = UUID(uuidString: "C3D3E5F6-1A2B-4C7D-8E9F-0A2B2C3D4E6F")!
-        let id = UUID(uuidString: "B3D3A5F6-1A3B-5C7D-8E9F-0A3B2C3D4E6F")!
+        static let name = "BestRestaurant007"
+        static let rating = 4
+        static let imageURL = "https://s3-media0.fl.yelpcdn.com/bphoto/rKctRFj8diqswEkATTDC5g/o.jpg"
+        static let partyID = FakeParty.id
+        static let id = UUID(uuidString: "B3D3A5F6-1A3B-5C7D-8E9F-0A3B2C3D4E6F")!
     }
 
     enum UserLevel {
